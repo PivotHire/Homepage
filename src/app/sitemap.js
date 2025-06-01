8962a23308ee9801d69521dd5746ffbf9808dc59
CLCK0622 <zhongyi070622@163.com>
2025-06-02 02:02:32 +0800

    feat:sitemap&robots.txt

---
+++ src/app/sitemap.js
@@ -0,0 +1,18 @@
+const BASE_URL = 'https://www.pivothire.tech/';
+
+export default async function sitemap() {
+
+    const staticPages = [
+        {
+            url: BASE_URL,
+            lastModified: new Date().toISOString(),
+            changeFrequency: 'monthly',
+            priority: 1.0,
+        }
+    ];
+
+    return [
+        ...staticPages,
+        ...dynamicPostUrls,
+    ];
+}
